41ebc56b5c79128d174672410d5138fbb5c0a7c0
Andrew-Gibson42 <andrewgibs4242@gmail.com>
2019-10-22 18:16:02 -0500

    Added visual to save_camera_frame.py

--- vision/realsense/save_camera_frame.py
+++ vision/realsense/save_camera_frame.py
@@ -35,31 +35,61 @@ profile = pipeline.start(config)
 depth_sensor = profile.get_device().first_depth_sensor()
 depth_scale = depth_sensor.get_depth_scale()
 
+# We will be removing the background of objects more than
+# clipping_distance_in_meters meters away
+clipping_distance_in_meters = 12 #1 meter (changed to 12)
+clipping_distance = clipping_distance_in_meters / depth_scale
+
 # Create an align object
 # rs.align allows us to perform alignment of depth frames to others frames
 # The "align_to" is the stream type to which we plan to align depth frames.
 align_to = rs.stream.color
 align = rs.align(align_to)
 
+while True:
+    # Get frameset of color and depth
+    frames = pipeline.wait_for_frames()
+    # frames.get_depth_frame() is a 640x360 depth image
 
-# Get frameset of color and depth
-frames = pipeline.wait_for_frames()
-# frames.get_depth_frame() is a 640x360 depth image
+    # Align the depth frame to color frame
+    aligned_frames = align.process(frames)
 
-# Align the depth frame to color frame
-aligned_frames = align.process(frames)
+    # Get aligned frames
+    aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+    color_frame = aligned_frames.get_color_frame()
 
-# Get aligned frames
-aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
-color_frame = aligned_frames.get_color_frame()
+    # Validate that both frames are valid
+    if not aligned_depth_frame or not color_frame:
+        continue
 
-depth_image = np.asanyarray(aligned_depth_frame.get_data())
-color_image = np.asanyarray(color_frame.get_data())
+    depth_image = np.asanyarray(aligned_depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
 
-time = str(datetime.datetime.now()).replace(' ', '_').replace(':', '.')
-cv2.imwrite(f"{time}-colorImage.jpg", color_image)
-cv2.imwrite(f"{time}-depthImage.jpg", depth_image)
-with open(f"{time}-depthScale.txt", 'w') as file:
-    file.write(str(depth_scale))
+    # Remove background - Set pixels further than clipping_distance to grey
+    grey_color = 153
+    depth_image_3d = np.dstack((depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
+    bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+
+    # Render images
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    images = np.hstack((bg_removed, depth_colormap))
+    cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('Align Example', images)
+    key = cv2.waitKey(1)
+    # Press the X button on the window to close the window
+    if (cv2.getWindowProperty('Align Example', 0) == -1):
+        cv2.destroyAllWindows()
+        break
+
+    # Press esc or 'q' to take the pictures and close the window
+    if key & 0xFF == ord('q') or key == 27:
+        time = str(datetime.datetime.now()).replace(' ', '_').replace(':', '.')
+        cv2.imwrite(f"{time}-colorImage.jpg", color_image)
+        cv2.imwrite(f"{time}-depthImage.jpg", depth_image)
+        with open(f"{time}-depthScale.txt", 'w') as file:
+            file.write(str(depth_scale))
+
+        cv2.destroyAllWindows()
+        break
 
 pipeline.stop()
