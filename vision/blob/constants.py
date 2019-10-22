e6cbde46966ca9dfc527f3954e96f9402c3c8842
Michael Pieper <michaelepieper@gmail.com>
2019-10-22 17:37:11 -0500

    Create file to hold constant values and enums

---
+++ vision/blob/constants.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class ObjectType(Enum):
+    AVOID = 'avoid'
+    PYLON = 'pylon'
+    MODULE = 'module'
+    BOAT = 'boat'
+    UNKNOWN = 'unknown'
+
+MIN_THRESHOLD = 100
+MAX_THRESHOLD = 500
+
+MIN_AREA = 100
+MAX_AREA = 200000
+
+MIN_CIRCULARITY = 0.785
+MAX_CIRCULARITY = 0.95
+
