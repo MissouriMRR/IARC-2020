fef3accc926a955a6ce62a5df5201014dc03278a
Andrew-Gibson42 <andrewgibs4242@gmail.com>
2019-10-24 18:58:39 -0500

    Fixed some errors

--- vision/realsense/save_camera_frame.py
+++ vision/realsense/save_camera_frame.py
@@ -1,8 +1,12 @@
 """
 This program will save a color frame as a JPG, the depth frame as a JPG,
-and the depth scale in a .txt file for use in image processing.
-"""
+and the depth scale in a .txt file for use in image processing. The realsense camera must be
+plugged into the computer, and must not be currently streaming for the program to work.
 
+The files are all saved in the same directory as the program, and are named using the current
+date and time at which the frames were taken, followed by either "-depthImage", "-colorImage",
+or "-depthScale" accordingly.
+"""
 
 # First import the library
 import pyrealsense2 as rs
@@ -13,83 +17,83 @@ import cv2
 # Import datetime for filename
 import datetime
 
-#Initialize resolution and framerate constants
-RESOLUTION_WIDTH = 640
-RESOLUTION_HEIGHT = 480
-FRAMERATE = 30
 
-# Create a pipeline
-pipeline = rs.pipeline()
+def save_frame_on_press(width, height, framerate, serial_no):
+    # Create a pipeline
+    pipeline = rs.pipeline()
 
-# Create a config and configure the pipeline to stream
-# different resolutions of color and depth streams (hypothetically)
-config = rs.config()
-#config.enable_device("846112073537") #a unique serial number for a camera, enables config to get data from a specific camera
-config.enable_stream(rs.stream.depth, RESOLUTION_WIDTH, RESOLUTION_HEIGHT, rs.format.z16, FRAMERATE)
-config.enable_stream(rs.stream.color, RESOLUTION_WIDTH, RESOLUTION_HEIGHT, rs.format.bgr8, FRAMERATE)
+    # Create a config and configure the pipeline to stream
+    # different resolutions of color and depth streams (hypothetically)
+    config = rs.config()
 
-# Start streaming
-profile = pipeline.start(config)
+    #a unique serial number for a camera, enables config to get data from a specific camera
+    if serial_no:
+        config.enable_device(serial_no)
 
-# Getting the depth sensor's depth scale (see rs-align example for explanation)
-depth_sensor = profile.get_device().first_depth_sensor()
-depth_scale = depth_sensor.get_depth_scale()
+    config.enable_stream(rs.stream.depth, width, height, rs.format.z16, framerate)
+    config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, framerate)
 
-# We will be removing the background of objects more than
-# clipping_distance_in_meters meters away
-clipping_distance_in_meters = 12 #1 meter (changed to 12)
-clipping_distance = clipping_distance_in_meters / depth_scale
+    # Start streaming
+    profile = pipeline.start(config)
 
-# Create an align object
-# rs.align allows us to perform alignment of depth frames to others frames
-# The "align_to" is the stream type to which we plan to align depth frames.
-align_to = rs.stream.color
-align = rs.align(align_to)
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
 
-while True:
-    # Get frameset of color and depth
-    frames = pipeline.wait_for_frames()
-    # frames.get_depth_frame() is a 640x360 depth image
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
 
-    # Align the depth frame to color frame
-    aligned_frames = align.process(frames)
+    while True:
+        # Get frameset of color and depth
+        frames = pipeline.wait_for_frames()
+        # frames.get_depth_frame() is a 640x360 depth image
 
-    # Get aligned frames
-    aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
-    color_frame = aligned_frames.get_color_frame()
+        # Align the depth frame to color frame
+        aligned_frames = align.process(frames)
 
-    # Validate that both frames are valid
-    if not aligned_depth_frame or not color_frame:
-        continue
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
 
-    depth_image = np.asanyarray(aligned_depth_frame.get_data())
-    color_image = np.asanyarray(color_frame.get_data())
+        # Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
+            continue
 
-    # Remove background - Set pixels further than clipping_distance to grey
-    grey_color = 153
-    depth_image_3d = np.dstack((depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
-    bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
 
-    # Render images
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-    images = np.hstack((bg_removed, depth_colormap))
-    cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('Align Example', images)
-    key = cv2.waitKey(1)
-    # Press the X button on the window to close the window
-    if (cv2.getWindowProperty('Align Example', 0) == -1):
-        cv2.destroyAllWindows()
-        break
+        # Remove background - Set pixels further than clipping_distance to grey
+        grey_color = 153
+        depth_image_3d = np.dstack((depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
+        bg_removed = np.where((depth_image_3d <= 0), grey_color, color_image)
 
-    # Press esc or 'q' to take the pictures and close the window
-    if key & 0xFF == ord('q') or key == 27:
-        time = str(datetime.datetime.now()).replace(' ', '_').replace(':', '.')
-        cv2.imwrite(f"{time}-colorImage.jpg", color_image)
-        cv2.imwrite(f"{time}-depthImage.jpg", depth_image)
-        with open(f"{time}-depthScale.txt", 'w') as file:
-            file.write(str(depth_scale))
+        # Render images
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        images = np.hstack((bg_removed, depth_colormap))
+        cv2.namedWindow('Depth/Color Stream', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('Depth/Color Stream', images)
+        key = cv2.waitKey(1)
+        # Press the X button on the window to close the window
+        if (cv2.getWindowProperty('Depth/Color Stream', 0) == -1):
+            cv2.destroyAllWindows()
+            break
 
-        cv2.destroyAllWindows()
-        break
+        # Press esc (27) or 'q' to take the pictures and close the window
+        if key == ord('q') or key == 27:
+            time = str(datetime.datetime.now()).replace(' ', '_').replace(':', '.')
+            cv2.imwrite(f"{time}-colorImage.jpg", color_image)
+            cv2.imwrite(f"{time}-depthImage.jpg", depth_image)
+            with open(f"{time}-depthScale.txt", 'w') as file:
+                file.write(str(depth_scale))
 
-pipeline.stop()
+            cv2.destroyAllWindows()
+            break
+
+    pipeline.stop()
+
+
+if __name__ == '__main__':
+    save_frame_on_press(640, 480, 30, '')
--- vision/realsense/stream_depth_color.py
+++ vision/realsense/stream_depth_color.py
@@ -1,5 +1,7 @@
 """
-This program will stream color and depth images to a window for the user.
+This program will align and stream color and depth images to a window for the user.
+For this program to run correctly, the realsense must be plugged in to the computer,
+and must not be currently streaming in another program/software.
 """
 
 # First import the library
@@ -9,73 +11,88 @@ import numpy as np
 # Import OpenCV for easy image rendering
 import cv2
 
-#Initialize resolution and framerate constants
-RESOLUTION_WIDTH = 640
-RESOLUTION_HEIGHT = 480
-FRAMERATE = 30
+if __name__ == '__main__':
+    #Initialize resolution and framerate constants
+    RESOLUTION_WIDTH = 640
+    RESOLUTION_HEIGHT = 480
+    FRAMERATE = 30
 
-# Create a pipeline
-pipeline = rs.pipeline()
+    #Initalize clipping constants
+    CLIPPING = False
+    clipping_distance_in_meters = 4
 
-# Create a config and configure the pipeline to stream
-# different resolutions of color and depth streams (hypothetically)
-config = rs.config()
-#config.enable_device("846112073537") #a unique serial number for a camera, enables config to get data from a specific camera
-config.enable_stream(rs.stream.depth, RESOLUTION_WIDTH, RESOLUTION_HEIGHT, rs.format.z16, FRAMERATE)
-config.enable_stream(rs.stream.color, RESOLUTION_WIDTH, RESOLUTION_HEIGHT, rs.format.bgr8, FRAMERATE)
+    #Initialize serial number constant-- if empty, no serial number is specified
+    SERIAL_NUMBER = ""
 
-# Start streaming
-profile = pipeline.start(config)
+    # Create a pipeline
+    pipeline = rs.pipeline()
 
-# Getting the depth sensor's depth scale (see rs-align example for explanation)
-depth_sensor = profile.get_device().first_depth_sensor()
-depth_scale = depth_sensor.get_depth_scale()
+    # Create a config and configure the pipeline to stream
+    # different resolutions of color and depth streams (hypothetically)
+    config = rs.config()
 
-# We will be removing the background of objects more than
-#  clipping_distance_in_meters meters away
-clipping_distance_in_meters = 4 #1 meter (changed to 4)
-clipping_distance = clipping_distance_in_meters / depth_scale
+    #a unique serial number for a camera, enables config to get data from a specific camera
+    if SERIAL_NUMBER:
+        config.enable_device(SERIAL_NUMBER)
 
-# Create an align object
-# rs.align allows us to perform alignment of depth frames to others frames
-# The "align_to" is the stream type to which we plan to align depth frames.
-align_to = rs.stream.color
-align = rs.align(align_to)
+    config.enable_stream(rs.stream.depth, RESOLUTION_WIDTH, RESOLUTION_HEIGHT, rs.format.z16, FRAMERATE)
+    config.enable_stream(rs.stream.color, RESOLUTION_WIDTH, RESOLUTION_HEIGHT, rs.format.bgr8, FRAMERATE)
 
-# Streaming loop
-while True:
-    # Get frameset of color and depth
-    frames = pipeline.wait_for_frames()
-    # frames.get_depth_frame() is a 640x360 depth image
+    # Start streaming
+    profile = pipeline.start(config)
 
-    # Align the depth frame to color frame
-    aligned_frames = align.process(frames)
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
 
-    # Get aligned frames
-    aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
-    color_frame = aligned_frames.get_color_frame()
+    # We will be removing the background of objects more than
+    #  clipping_distance_in_meters meters away
+    if CLIPPING:
+        clipping_distance = clipping_distance_in_meters / depth_scale
 
-    # Validate that both frames are valid
-    if not aligned_depth_frame or not color_frame:
-        continue
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
 
-    depth_image = np.asanyarray(aligned_depth_frame.get_data())
-    color_image = np.asanyarray(color_frame.get_data())
+    # Streaming loop
+    while True:
+        # Get frameset of color and depth
+        frames = pipeline.wait_for_frames()
+        # frames.get_depth_frame() is a 640x360 depth image
 
-    # Remove background - Set pixels further than clipping_distance to grey
-    grey_color = 153
-    depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
-    bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+        # Align the depth frame to color frame
+        aligned_frames = align.process(frames)
 
-    # Render images
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-    images = np.hstack((bg_removed, depth_colormap))
-    cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('Align Example', images)
-    key = cv2.waitKey(1)
-    # Press esc or 'q' to close the image window
-    if key & 0xFF == ord('q') or key == 27 or cv2.getWindowProperty('Align Example', 0) == -1:
-        cv2.destroyAllWindows()
-        break
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
 
-pipeline.stop()
+        # Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
+            continue
+
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # Remove background - Set pixels further than clipping_distance to grey
+        grey_color = 153
+        depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+        if CLIPPING:
+            bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+        else:
+            bg_removed = np.where((depth_image_3d <= 0), grey_color, color_image)
+
+        # Render images
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        images = np.hstack((bg_removed, depth_colormap))
+        cv2.namedWindow('Depth/Color Stream', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('Depth/Color Stream', images)
+        key = cv2.waitKey(1)
+        # Press esc or 'q' to close the image window
+        if key == ord('q') or key == 27 or cv2.getWindowProperty('Depth/Color Stream', 0) == -1:
+            cv2.destroyAllWindows()
+            break
+
+    pipeline.stop()
