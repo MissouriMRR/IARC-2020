933b064ea5494da751097abbfd6ffa8bb3ae6722
Michael Pieper <michaelepieper@gmail.com>
2019-10-24 18:33:01 -0500

    Update docstring

--- vision/unit_tests/test_blobbing.py
+++ vision/unit_tests/test_blobbing.py
@@ -9,21 +9,22 @@ class TestBlobbing(unittest.TestCase):
 
         Settings
         --------
-        :var samples_directory: relative path to a directory of sample images
-        :var expected_blobs: list of expected blobs to be found in each image
-        :var logging: bool for whether to output details about the assertion
+        expected_blobs: list[int]
+            expected blobs to be found in each image
+        logging: bool
+            whether to output details about the assertion
 
         Returns
         -------
-        :return ndarray[bool] Whether each image in has the correct number of
+        list[bool]
+            whether the expected number of blobs in each image equals the detected number of blobs
         """
-        samples_directory = '../blob/samples'
         expected_blobs = [1, 30, 1, 1]
-        logging = False
+        logging = True
         found_blobs = []
         vision_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-        for img in os.listdir(samples_directory):
+        for img in os.listdir('../blob/samples'):
             img_file = os.path.join(vision_folder, 'blob', 'samples', img)
             keypoints = find_blobs(img_file, logging)
             found_blobs.append(len(keypoints))
