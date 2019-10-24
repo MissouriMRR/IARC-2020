bc925227875ecc52c039986c8ea11fde65411fff
Michael Pieper <michaelepieper@gmail.com>
2019-10-24 18:21:22 -0500

    Add option to enable logging image info, fix path to samples directory

--- vision/unit_tests/test_blobbing.py
+++ vision/unit_tests/test_blobbing.py
@@ -1,6 +1,5 @@
 import unittest
 import os
-import numpy as np
 from vision.blob.blobfind import find_blobs
 
 class TestBlobbing(unittest.TestCase):
@@ -12,18 +11,27 @@ class TestBlobbing(unittest.TestCase):
         --------
         :var samples_directory: relative path to a directory of sample images
         :var expected_blobs: list of expected blobs to be found in each image
+        :var logging: bool for whether to output details about the assertion
 
         Returns
         -------
         :return ndarray[bool] Whether each image in has the correct number of
         """
         samples_directory = '../blob/samples'
-        expected_blobs = np.array([1, 30, 1, 1])
-        found_blobs = np.array([])
-        for i, img in enumerate(os.listdir(samples_directory)):
-            img_file = samples_directory + os.fsdecode(img)
-            keypoints = find_blobs(img_file)
-            np.append(found_blobs, len(keypoints))
+        expected_blobs = [1, 30, 1, 1]
+        logging = False
+        found_blobs = []
+        vision_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        for img in os.listdir(samples_directory):
+            img_file = os.path.join(vision_folder, 'blob', 'samples', img)
+            keypoints = find_blobs(img_file, logging)
+            found_blobs.append(len(keypoints))
+
+        if logging:
+            print("Expected:", expected_blobs)
+            print("Found:", found_blobs)
+
         self.assertLessEqual(list(expected_blobs), list(found_blobs))
 
 
