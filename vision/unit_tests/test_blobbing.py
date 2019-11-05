c2de652d6ca55e6c5ca78f964e1facdbc03f06c3
Michael Pieper <michaelepieper@gmail.com>
2019-11-05 16:58:21 -0600

    Rework unit test to perform subtests for each image

--- vision/unit_tests/test_blobbing.py
+++ vision/unit_tests/test_blobbing.py
@@ -11,8 +11,6 @@ class TestBlobbing(unittest.TestCase):
         --------
         expected_blobs: list[int]
             expected blobs to be found in each image
-        logging: bool
-            whether to output details about the assertion
 
         Returns
         -------
@@ -20,21 +18,14 @@ class TestBlobbing(unittest.TestCase):
             whether the expected number of blobs in each image equals the detected number of blobs
         """
         expected_blobs = [1, 30, 1, 1]
-        logging = True
         found_blobs = []
         vision_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-        for img in os.listdir('../blob/samples'):
-            img_file = os.path.join(vision_folder, 'blob', 'samples', img)
-            keypoints = find_blobs(img_file, logging)
-            found_blobs.append(len(keypoints))
-
-        if logging:
-            print("Expected:", expected_blobs)
-            print("Found:", found_blobs)
-
-        self.assertLessEqual(list(expected_blobs), list(found_blobs))
-
+        for i, img in enumerate(os.listdir('../blob/samples')):
+            with self.subTest(i=img):
+                img_file = os.path.join(vision_folder, 'blob', 'samples', img)
+                keypoints = find_blobs(img_file, logging=True)
+                self.assertEqual(len(keypoints), expected_blobs[i], msg=f"Expected {expected_blobs[i]} blobs, found {len(keypoints)} in image {img}")
 
 if __name__ == '__main__':
     unittest.main()
