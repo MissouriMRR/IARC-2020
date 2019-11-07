614c19160b05930794d0e899a3ddb594e94776a4
Michael Pieper <michaelepieper@gmail.com>
2019-11-07 17:56:10 -0600

    Update config names to match OpenCV naming convention

--- vision/unit_tests/test_blobbing.py
+++ vision/unit_tests/test_blobbing.py
@@ -1,6 +1,12 @@
 import unittest
 import os
-from vision.blob.blobfind import find_blobs
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+try:
+    from vision.blob.blobfind import BlobFinder
+except ImportError:
+    from blob.blobfind import BlobFinder
 
 class TestBlobbing(unittest.TestCase):
     def test_finding_blobs(self):
@@ -18,14 +24,14 @@ class TestBlobbing(unittest.TestCase):
             whether the expected number of blobs in each image equals the detected number of blobs
         """
         expected_blobs = [1, 30, 1, 1]
-        found_blobs = []
         vision_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
         for i, img in enumerate(os.listdir('../blob/samples')):
             with self.subTest(i=img):
                 img_file = os.path.join(vision_folder, 'blob', 'samples', img)
-                keypoints = find_blobs(img_file, logging=True)
-                self.assertEqual(len(keypoints), expected_blobs[i], msg=f"Expected {expected_blobs[i]} blobs, found {len(keypoints)} in image {img}")
+                detector = BlobFinder(img_file)
+                bounding_boxes = detector.find()
+                self.assertEqual(len(bounding_boxes), expected_blobs[i], msg=f"Expected {expected_blobs[i]} blobs, found {len(bounding_boxes)} in image {img}")
 
 if __name__ == '__main__':
     unittest.main()
