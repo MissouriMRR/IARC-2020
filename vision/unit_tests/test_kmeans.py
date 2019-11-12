e41781319d97ebd9e78a33a2880562d5f9e0989c
Faolan Cody <faolan.cody@gmail.com>
2019-11-12 17:46:35 -0600

    Updated structure and organization
    Added README.md and requirements.txt. Updated unittest class and made some minor changes to ModuleKMeans.py.

---
+++ vision/unit_tests/test_kmeans.py
@@ -0,0 +1,26 @@
+import unittest
+from ModuleKMeans import ModuleKMeans as mkm
+
+class TestKMeans(unittest.TestCase):
+
+    def test_fractal(self):
+        """
+        Testing Fractal KMeans Display
+
+        Test images must be put in a file called images
+        """
+
+        pass
+
+    def test_binary(self):
+        """
+        Testing Binary KMeans Display
+
+        Test images must be put in a file called images
+        """
+
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
