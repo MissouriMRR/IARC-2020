df5593fb5f86840c24e37810eb66d1b0c2815138
fallscameron01 <camfalls0201@gmail.com>
2019-11-12 19:40:44 -0600

    Update ModuleInFrame.py

--- vision/module_in_frame/ModuleInFrame.py
+++ vision/module_in_frame/ModuleInFrame.py
@@ -5,27 +5,25 @@ This file contains the ModuleInFrame function to detect if the module is in an i
 import cv2
 import numpy as np
 
-def ModuleInFrame(imgPath):
+def ModuleInFrame(img):
     """
     Determines if the Module is in frame
 
     Parameters
     ----------
-    imgPath, is a string that is the path of the image to be passed into the function
+    img: numpy array
+        The image stored in a numpy array.
 
     Returns
     -------
-    isInFrame, a bool, which is true if the module is in the frame and false if not in the frame
+    bool: true if the module is in the frame and false if not in the frame
     """
 
     # Ignore numpy warnings
     np.seterr(all="ignore")
 
-    # Get target image
-    img = cv2.imread(imgPath)
-
     # Remove depth channel
-    img.reshape((-1, 3))
+    img = img[:, :, :3]
 
     # Grayscale
     gray = cv2.cvtColor(src=img, code=cv2.COLOR_RGB2GRAY)
@@ -35,34 +33,32 @@ def ModuleInFrame(imgPath):
 
     # Laplacian Transform
     laplacian = cv2.Laplacian(src=blur, ddepth=cv2.CV_8U, ksize=3)
-    laplacian = np.uint8(np.around(laplacian))
+    laplacian = np.uint8(laplacian)
 
     # Hough Circle Detection
     circles = cv2.HoughCircles(image=laplacian, method=cv2.HOUGH_GRADIENT, dp=1, minDist=8, param1=50, param2=40, minRadius=0, maxRadius=50)
-    circles = np.uint16(np.around(circles))
+    circles = np.uint16(circles)
+
+    # Resize circles into 2d array
+    circles = np.reshape(circles, (np.shape(circles)[1], 3))
 
     # Finding slopes between the circles
-    slopes = { }
-    for circle in circles:
-        for (x, y, r) in circle:
-            for iCircle in circles:
-                for (iX, iY, iR) in iCircle:
-                    m = (iY - y) / (iX - x)
-                    if(m not in slopes):
-                        slopes[m] = 0
-                    slopes[m] += 1
+    slopes = []    
+    for (x, y, r) in circles:
+        for (iX, iY, iR) in circles:
+            m = (iY - y) / (iX - x)
+            
+            # slope must be non-infinite and can't be between the same circle
+            if (not np.isnan(m)) and (not np.isinf(m)) and (x != iX and y != iY):
+                slopes.append(m)
 
-    # Counting the number of parallel slopes
-    parallels = 0
-    for(key, val) in slopes.items():
-        if(val > 1):
-            parallels += val
-        for (key2, val2) in slopes.items():
-            if(abs(key - key2) < .01 and key != key2):
-                parallels += 1
-
-    # Determine if module is in frame
-    isInFrame = False
-    if(parallels > 300):
-        isInFrame = True
-    return isInFrame
+    # Bucket sorting slopes to group parallels
+    slopes = np.array(np.abs(slopes))
+    upper = np.amax(slopes) 
+    lower = np.amin(slopes)
+    BUCKET_MODIFIER = 1
+    num_buckets = np.int32(upper - lower) * BUCKET_MODIFIER
+    buckets, _ = np.histogram(slopes, num_buckets, (lower, upper))
+    
+    # Determine if any bucket of slopes is big enough
+    return any(buckets > 25)
