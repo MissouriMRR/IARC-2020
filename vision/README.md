05d7105e63210c0dfa9e397e5aeeaaa99d3fc536
Liem Thanh Ho <lthpkh@mst.edu>
2019-11-14 22:27:36 -0600

    made changes for feedback pull request #2

--- vision/README.md
+++ vision/README.md
@@ -136,20 +136,4 @@ There should be a test case for each major function of each class/function.
 ```
 
 #### Mock classes
-# Detect Russian Word: 'модули иртибот'
-#Download pytesseract for OCR (optical character recognition)
-
-#-----Using Mac and Homebrew-----
-#(1) Downlaod tesseract:
-		brew install tesseract
-
-#(2) Download tesseract-lang (to use Russian)
-		brew install tesseract-lang
-#(3) Download Python wrapper
-        pip install pytesseract
-
-#-----Using Linux/Windows-----
-#(1) Downlaod tesseract and language:
-    sudo apt-get install tesseract-ocr-[rus]
-#(2) Download Python wrapper
-    pip install pytesseract
+-TODO 
---
+++ vision/unit_tests/test_boat.py
@@ -0,0 +1,38 @@
+import unittest
+import os
+import cv2
+from vision.boat.detectRussianWord import detect_russian_word
+
+class TestDetectRussianWord(unittest.TestCase):      
+    def test_detect_russian(self):
+        """
+        Testing vision.boat.detectRussianWord
+
+        Settings
+        --------
+        detectRussianWord.originalImage: PNG
+            Stores image into a variable.
+        detectRussianWord.text: string
+            Stores text grabbed from image.
+
+        Returns
+        -------
+        True if text grabbed from image matches 'модули иртибот'.
+        False if not.
+        """
+        # Ensure 'модули иртибот' is found within image
+        expected = {
+            'boat1.jpg': 1,
+            'notboat.jpg': 0
+        }
+
+        vision_folder = 'vision' if os.isdir('vision') else ''
+
+        for filename, expected_result in expected.items():
+            image = cv2.imread(os.path.join(vision_folder, filename))
+            result = detect_russian_word(image)
+            self.assertEqual(expected_result, result, msg=f"Failed at image {filename}")
+
+    
+if __name__ == '__main__':
+    unittest.main()
