4a9661ffe1ecee77c5b64a1bf043a374ecbdab6a
Ayden Mason <42448386+aydenmason@users.noreply.github.com>
2020-01-17 03:45:48 -0600

    adding multiprocessing
    Multiprocessing functionality added so that the vision and flight code can read/write to the same object.

--- vision/interface.py
+++ vision/interface.py
@@ -1,7 +1,7 @@
 
 """The Environment class will manage the other objects in the environment"""
-
-
+import multiprocessing as mp 
+from functools import partial
 class Environment:
 
     #list of rectangles in environment
@@ -23,3 +23,18 @@ class Environment:
     #updates rects list
     def update(self, bounding_boxes):
         self.bounding_boxes = bounding_boxes
+
+    #This is the functiont that the teams should call
+    #safe_update handles all multiprocessing options
+    #CAUTION: may not run only work on Linux/MacOS  
+    def safe_update(self, bounding_boxes):
+        pool = mp.Pool()
+        m = mp.Manager()
+        l = m.Lock()
+        func = partial(self.update(bounding_boxes), l)
+        pool.map(func, bounding_boxes)
+        pool.close()
+        pool.join()
+
+
+  
