90c6eafd66848e31e93c26be0db7d6e283dda907
Michael Pieper <michaelepieper@gmail.com>
2020-02-04 17:54:33 -0600

    Fix blob timer

--- vision/blob/blob_timer.py
+++ vision/blob/blob_timer.py
@@ -10,17 +10,18 @@ import timeit
 from vision.blob.blobfind import BlobFinder
 from vision.util.import_params import import_params
 
-prefix = 'vision' if os.path.isdir("vision") else ''
-img_folder = os.path.join(prefix, 'vision_images', 'blob')
-config_filename = os.path.join(prefix, 'blob', 'config.json')
+if __name__ == '__main__':
+    prefix = 'vision' if os.path.isdir("vision") else ''
+    img_folder = os.path.join(prefix, 'vision_images', 'blob')
+    config_filename = os.path.join(prefix, 'blob', 'config.json')
 
-with open(config_filename, 'r') as config_file:
-    config = json.load(config_file)
+    with open(config_filename, 'r') as config_file:
+        config = json.load(config_file)
 
-for img in os.listdir(img_folder):
-    if img[-4:] not in ['.png', '.jpg']:
-        continue
+    for img in os.listdir(img_folder):
+        if img[-4:] not in ['.png', '.jpg']:
+            continue
 
-    image = cv2.imread(os.path.join(img_folder, os.fsdecode(img)))
-    blob_finder = BlobFinder(image, params=import_params(config))
-    print(img, image.shape, timeit.timeit(blob_finder.find, number=10) / 10)
+        image = cv2.imread(os.path.join(img_folder, os.fsdecode(img)))
+        blob_finder = BlobFinder(params=import_params(config))
+        print(img, image.shape, timeit.timeit(lambda: blob_finder.find(image), number=10) / 10)
