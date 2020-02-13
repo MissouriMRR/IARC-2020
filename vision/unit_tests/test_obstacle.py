32e4af78f0514cc512055ec9bc3e53d50dd0b734
csdhv9 <csdhv9@umsystem.edu>
2020-02-12 23:42:28 -0600

    Update obstacle unit test.

--- vision/unit_tests/test_obstacle.py
+++ vision/unit_tests/test_obstacle.py
@@ -1,133 +1,110 @@
 """
 Obstacle detector unit tests.
 """
-import unittest
-import os
-import sys
-import json
-
-import numpy as np
-import cv2
-import lxml.etree
-
+import os, sys
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 gparent_dir = os.path.dirname(parent_dir)
 ggparent_dir = os.path.dirname(gparent_dir)
 sys.path += [parent_dir, gparent_dir, ggparent_dir]
 
+import unittest
+
+import numpy as np
+import cv2
+
 from vision.obstacle.obstacle_finder import ObstacleFinder
-from vision.util.import_params import import_params
+from vision.bounding_box import BoundingBox, ObjectType
 
 
 class TestObstacleDetection(unittest.TestCase):
     """
     Testing obstacle detection.
     """
-    def test_obstacle_detection(self):
+    def _get_params(self, **kwargs):
         """
-        Tests that the expected number of obstacles is found
+        Generate a blob detector params object.
+        """
+        config = {
+            "minThreshold": 10,
+            "maxThreshold": 300,
+            "filterByArea": True,
+            "minArea": 100,
+            "maxArea": 200000
+        }
+        config.update(kwargs)
+
+        params = cv2.SimpleBlobDetector_Params()
+
+        for key, value in config.items():
+            setattr(params, key, value)
+
+        return params
+
+    def test_find(self):
+        """
+        Testing ObstacleFinder.find.
+
+        Parameters
+        ----------
+        image: ndarray
 
         Settings
         --------
-        expected_obstacles: dict{string: int}
-            number of expected obstacles (value) to be found in each image (key)
+        _params: cv2.SimpleBlobDetector_Params
+            Parameters for blob_detector.
+        blob_detector: cv2.SimpleBlobDetector_create(self.params)
+            Blob detection algorithm.
 
         Returns
         -------
-        list[bool]
-            whether the expected number of obstacles in each image equals the detected number of obstacles
+        list[BoundingBox]
         """
-        expected_obstacles = {
-            "apple.jpg": 1,
-            "legos.jpg": 30,
-            "MyBeach.png": 1,
-            "oranges.png": 1,
-            "sampleobj.png": 1
-        }
-        prefix = 'vision' if os.path.isdir("vision") else ''
+        ### Ensure range of Parameters work
+        IMAGE_SIZE = (1920, 1080)
 
-        config_filename = os.path.join(prefix, 'obstacle', 'config.json')
-        with open(config_filename, 'r') as config_file:
-            raw_config = json.load(config_file)
+        ## 3 Channel {0..255} Image
+        with self.subTest(i="3 Channel {0..255} Image"):
+            detector = ObstacleFinder(params=self._get_params())
 
-        config = import_params(raw_config)
+            image = np.random.randint(0, 255, size=(*IMAGE_SIZE, 3), dtype='uint8')
 
-        for filename, expected in expected_obstacles.items():
-            with self.subTest(i=filename):
-                img_filename = os.path.join(prefix, 'vision_images', 'obstacle', filename)
-                img_file = cv2.imread(img_filename)
+            detector.find(image)
 
-                detector = ObstacleFinder(params=config)
-                bounding_boxes = detector.find(img_file)
+        ### Ensure returns correct type of BoundingBox
+        with self.subTest(i="Object type"):
+            detector = ObstacleFinder(params=self._get_params())
 
-                self.assertEqual(len(bounding_boxes), expected, msg=f"Expected {expected} obstacles, found {len(bounding_boxes)} in image {filename}")
+            for i in range(1, 9):
+                image = np.zeros((1000, 1000, 3), dtype='uint8')
+                cv2.rectangle(image, (200, 200), (200 + (i * 100), 200 + (i * 100)), (255, 255, 255), 2)
 
-    def test_annotation_accuracy(self):
-        """
-        Test accuracy of obstacle finder via custom Annotations w/ blob_annotator tool.
-        """
-        THRESHOLD = 5
+                output = detector.find(image)
 
-        prefix = 'vision' if os.path.isdir("vision") else ''
-        img_folder = os.path.join(prefix, 'vision_images', 'obstacle')
-        annotation_folder = os.path.join(img_folder, 'Annotations')
+                if output is not None:
+                    break
+            else:
+                self.fail(msg="Failed to detect obstacle to be able to test.")
 
-        ## Read annotations
-        if not os.path.isdir(annotation_folder):
-            print(f"No annotation folder found! -- {annotation_folder}")
-            return
+            for box in output:
+                self.assertIsInstance(box, BoundingBox)
 
-        annotations = {filename: lxml.etree.parse(os.path.join(annotation_folder, filename)).getroot() for filename in os.listdir(annotation_folder)}
+                ##
+                self.assertIn(len(box.vertices), [4, 8])
 
-        print(f"Found {len(annotations)} annotations in vision_images/obstacle!")
+                X, Y = [], []
+                for vertex in box.vertices:
+                    self.assertIsInstance(vertex, tuple)
 
-        ## Check accuracy of obstacle detector
-        config_filename = os.path.join(prefix, 'obstacle', 'config.json')
-        with open(config_filename, 'r') as config_file:
-            raw_config = json.load(config_file)
+                    X.append(vertex[0])
+                    Y.append(vertex[1])
 
-        config = import_params(raw_config)
+                self.assertGreater(max(X) - min(X), 50)
+                self.assertGreater(max(Y) - min(Y), 50)
 
-        for _, annotation in annotations.items():
-            filename = annotation.find('path').find('value').text
+                ##
+                self.assertIsInstance(box.object_type, ObjectType)
+                self.assertEqual(box.object_type, ObjectType.AVOID)
 
-            with self.subTest(i=filename):
-                img_path = os.path.join(img_folder, filename)
-                image = cv2.imread(img_path)
-
-                assert image.shape, f"Failed to read {img_path}!"
-
-                bounding_boxes = ObstacleFinder(params=config).find(image)
-
-                accuracy = 0
-
-                for value in annotation.findall('object'):
-                    annotation_bounding_box = value.find('bndbox')
-
-                    ax1, ay1, ax2, ay2 = [int(annotation_bounding_box.find(param).text) for param in ['xmin', 'ymin', 'xmax', 'ymax']]
-
-                    for bounding_box in bounding_boxes:
-                        ## Get x's and y's from bounding box
-                        X, Y, Z = [], [], []
-                        for x, y, z in bounding_box.vertices:
-                            X.append(x)
-                            Y.append(y)
-                            Z.append(z)
-                        X, Y = np.unique(X), np.unique(Y)
-                        bx1, by1, bx2, by2 = min(X), min(Y), max(X), max(Y)
-
-                        ## see if bx1, by1,... within +/- threshold of each ax1, ...
-
-                        x1_close = bx1 - THRESHOLD <= ax1 <= bx1 + THRESHOLD
-                        y1_close = by1 - THRESHOLD <= ay1 <= by1 + THRESHOLD
-                        x2_close = bx2 - THRESHOLD <= ax2 <= bx2 + THRESHOLD
-                        y2_close = by2 - THRESHOLD <= ay2 <= by2 + THRESHOLD
-
-                        if all((x1_close, y1_close, x2_close, y2_close)):
-                            accuracy += 1
-
-            accuracy /= len(annotation.findall('object'))
-            print(f"{filename}: {accuracy * 100:.2f}%")
 
 if __name__ == '__main__':
     unittest.main()
