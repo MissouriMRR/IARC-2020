c9e35dbb685f471b1f09c96fb428c7a0d19b602f
Michael Pieper <michaelepieper@gmail.com>
2020-02-13 18:01:44 -0600

    Fix naming

--- vision/blob/video_detector.py
+++ vision/blob/video_detector.py
@@ -12,14 +12,19 @@ class ObstacleVideo:
     @property
     def blob_finder(self):
         """
-        This function is called every time self.blob_finder = value"""
+        This function is called every time self.blob_finder is run
+        """
         return self._blob_finder
 
     @blob_finder.setter
-    def params(self, value):
+    def blob_finder(self, value):
+        """
+        Defines behavior of self.blob_finder = value
+        """
         if not isinstance(value, BlobFinder):
             raise ValueError(f"Requires instance of BlobFinder, got {type(value)}")
-        self.blob_finder = value
+        self._blob_finder = value
 
     def find_obstacles(self, video):
-        pass
+        for frame in video:
+            self.blob_finder.find(frame)
