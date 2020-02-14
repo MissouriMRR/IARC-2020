bbc28e1d02cf2728ddbcdb2d60bc5b994a127f66
Michael Pieper <michaelepieper@gmail.com>
2020-02-13 18:23:14 -0600

    Prototype main function

--- vision/blob/video_detector.py
+++ vision/blob/video_detector.py
@@ -2,11 +2,14 @@
 Detects obstacles in videos using ObstacleFinder
 """
 
+import os
+import json
 from vision.blob.blobfind import BlobFinder
+from vision.util.import_params import import_params
 
 class ObstacleVideo:
 
-    def __init__(self, blob_finder=None):
+    def __init__(self, blob_finder):
         self.blob_finder = blob_finder
 
     @property
@@ -28,3 +31,17 @@ class ObstacleVideo:
     def find_obstacles(self, video):
         for frame in video:
             self.blob_finder.find(frame)
+
+if __name__ == '__main__':
+    prefix = 'vision' if os.path.isdir("vision") else ''
+    vid_folder = os.path.join(prefix, 'vision_videos', 'obstacle')
+    config_filename = os.path.join(prefix, 'blob', 'config.json')
+
+    with open(config_filename, 'r') as config_file:
+        config = json.load(config_file)
+
+    blob_finder = BlobFinder(params=import_params(config))
+    video = ObstacleVideo(blob_finder)
+
+    for vid_file in os.listdir(vid_folder):
+        video.find_obstacles(vid_file)
