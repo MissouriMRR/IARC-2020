6606eb79e80e7469054fe053e4451d4996fd216c
Michael Pieper <michaelepieper@gmail.com>
2020-02-27 17:41:07 -0600

    Update ObstacleVideo

--- vision/blob/video_detector.py
+++ vision/blob/video_detector.py
@@ -3,9 +3,17 @@ Detects obstacles in videos using ObstacleFinder
 """
 
 import os
+import sys
+
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+gparent_dir = os.path.dirname(parent_dir)
+ggparent_dir = os.path.dirname(gparent_dir)
+sys.path += [parent_dir, gparent_dir, ggparent_dir]
+
 import json
 from vision.blob.blobfind import BlobFinder
 from vision.util.import_params import import_params
+from vision.camera.bag_file import BagFile
 
 class ObstacleVideo:
 
@@ -43,5 +51,15 @@ if __name__ == '__main__':
     blob_finder = BlobFinder(params=import_params(config))
     video = ObstacleVideo(blob_finder)
 
+    if os.path.exists("fieldsafe1.bag"):
+        bag_file = BagFile(500, 500, 60, "sampleFrames.bag")
+        for frame in bag_file:
+            print(frame)
+    else:
+        raise ValueError("what")
+
     for vid_file in os.listdir(vid_folder):
-        video.find_obstacles(vid_file)
+        print(vid_file)
+
+
+    #video.find_obstacles(vid_file)
