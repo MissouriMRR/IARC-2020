c404d28e989a44200f44c881fa2ced4fef9b518b
csdhv9 <csdhv9@umsystem.edu>
2020-02-27 22:17:55 -0600

    Basic implementation of bench pylon.

--- vision/benchmarks/times/bench_pylon.py
+++ vision/benchmarks/times/bench_pylon.py
@@ -1,55 +1,44 @@
 """
 Runs through images and determines which have the pylon
 """
-import unittest
-import os
-import sys
-import cv2
-
+import os, sys
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 gparent_dir = os.path.dirname(parent_dir)
 ggparent_dir = os.path.dirname(gparent_dir)
 sys.path += [parent_dir, gparent_dir, ggparent_dir]
 
-# from vision.pylon.detect_pylon import import_params
+import cv2
+
 from vision.pylon.detect_pylon import detect_red
 
 
-class TimePylon(unittest.TestCase):
-    def test_pylon(self):
+class TimePylon:
+    """
+    Timing all functionality of pylon project.
+    """
+    def setup(self):
         """
-        Tests whether or not the pylon's red is detected in the image
-
-        Settings
-        --------
-        expected_blobs: dict{string: int}
-            number of expected blobs (value) to be found in each image (key)
-
-        Returns
-        -------
-        list[bool]
-            whether the expected number of blobs in each image equals the detected number of blobs
+        Load images.
         """
-        expected_pylon = {
-            "sim_pylon.png":True,
-            "sim_pylon2.png":True,
-            "sim_pylon3.png":True
-        }
-        prefix = 'vision' if os.path.isdir("vision") else '..'
+        prefix = '' if os.path.isdir("times") else '..'
 
-        for filename, expected in expected_pylon.items():
-            with self.subTest(i=filename):
-                img_filename = os.path.join(prefix, 'vision_images', 'pylon', filename)
-                img_file = cv2.imread(img_filename)
-                if img_file is None:
-                    self.fail(msg="Failed to read image.  " + img_filename)
+        ## Load images
+        img_folder = os.path.join(prefix, '..', 'vision_images', 'pylon')
 
-                # detector = ObstacleFinder(img_file, params=config)
-                # bounding_boxes = detector.find()
-                pylon = detect_red(img_file)
+        self.images = []
+        for filename in os.listdir(img_folder):
+            if filename[-4:] not in ['.png', '.jpg']:
+                continue
 
-                self.assertEqual(pylon, expected, msg=f"Expected {expected} blobs, did not find pylon in image {filename}")
+            img_path = os.path.join(img_folder, os.fsdecode(filename))
 
+            image = cv2.imread(img_path)
 
-if __name__ == '__main__':
-    unittest.main()
+            self.images.append((image, None))
+
+    def time_in_frame(self):
+        """
+        Timing the pylon in frame detector.
+        """
+        for color_image, depth_image in self.images:
+            detect_red(color_image, depth_image)
