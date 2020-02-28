b746e11bdb3cbb74a260956b6439c697dd1aeb50
csdhv9 <csdhv9@umsystem.edu>
2020-02-27 21:46:35 -0600

    Fully implement runall.

--- vision/benchmarks/times/runall.py
+++ vision/benchmarks/times/runall.py
@@ -1,24 +1,37 @@
 """
-Import all classes via __init__.py
-
-For each class, 
- for time_ method in class:
-     run class.setup()
-     
-     run timeit.timeit(lambda: class.time_...(), number=5) / 5
+Run time benchmarks.
 """
+import timeit
 from times import modules
 
+
+N_REPEATS = 100
 
 if __name__ == '__main__':
     benchmarks = {}
 
+    ## Find time benchmarks in modules
     for module in modules:
-        #print(benchmark.__dict__)
-
         for key, value in module.__dict__.items():
             if key[:4] == 'Time':
                 benchmarks.update({key: value})
 
-    for key, benchmark in benchmarks.items():
-        print(key, benchmark)
+    ## Run benchmarks
+    for b_name, benchmark in benchmarks.items():
+        b_instance = benchmark()
+
+        try:
+            b_instance.setup()
+        except AttributeError:
+            print(f"No setup method found for {b_name}.")
+
+        for m_name, method in benchmark.__dict__.items():
+            if 'time_' not in m_name or not callable(method):
+                continue
+            
+            try:
+                time = timeit.timeit(lambda: method(b_instance), number=N_REPEATS) / N_REPEATS
+            except Exception:
+                print(f"{b_name}.{m_name}: error")
+            else:
+                print(f"{b_name}.{m_name}: {time:.5f}s")
