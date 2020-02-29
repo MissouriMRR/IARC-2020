34873ed54b3de68f5e0442a680fb5dae7e9637ff
csdhv9 <csdhv9@umsystem.edu>
2020-02-29 03:50:32 -0600

    Fix 0 slopes error.

--- vision/module/in_frame.py
+++ vision/module/in_frame.py
@@ -66,6 +66,9 @@ def ModuleInFrame(color_image, depth_image):
     # Converting slopes to degrees
     slopes = np.degrees(np.arctan(slopes))
 
+    if not slopes.size:
+        return False
+
     # Bucket sorting slopes to group parallels
     upper_bound = np.amax(slopes)
     lower_bound = np.amin(slopes)
