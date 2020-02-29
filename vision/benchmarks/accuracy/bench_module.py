58a5c34ed5a84b04bd55311a549c16f0e3efebe9
csdhv9 <csdhv9@umsystem.edu>
2020-02-29 01:48:48 -0600

    Setup accuracy benchmarks for all.

---
+++ vision/benchmarks/accuracy/bench_module.py
@@ -0,0 +1,30 @@
+"""
+For testing all module algorithms.
+"""
+import os, sys
+
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+gparent_dir = os.path.dirname(parent_dir)
+ggparent_dir = os.path.dirname(gparent_dir)
+sys.path += [parent_dir, gparent_dir, ggparent_dir]
+
+from vision.module.in_frame import ModuleInFrame as mif
+
+
+IMG_FOLDER = 'module'
+
+class AccuracyModuleInFrame:
+    """
+    Testing module.in_frame accuracy.
+    """
+    def accuracy_ModuleInFrame(self, color_image, depth_image):
+        """
+        Measuring accuracy of ModuleInFrame
+
+        Returns
+        -------
+        List[BoundingBox]
+        """
+        bounding_boxes = mif(color_image, depth_image)
+
+        return bounding_boxes
