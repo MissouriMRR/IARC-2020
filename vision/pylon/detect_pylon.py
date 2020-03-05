9082643e4cc5292ea448d8cd1fb4b5734f3c2ac2
Cameron Falls <camfalls0201@gmail.com>
2020-03-05 17:42:28 -0600

    Sum with numpy

--- vision/pylon/detect_pylon.py
+++ vision/pylon/detect_pylon.py
@@ -38,14 +38,9 @@ def detect_red(color_image, depth_image):
     # Red_mask picks out any pixel between the lower and upper limits
     # and replaces them with white.  Everything else is replaced with black
     red_mask = cv2.inRange(hsv, LOWER_RED, UPPER_RED)
-
     # Run through each pixel in the mask. If over RED_THRESHOLD is white
     # then we have detected a pylon
-    red_pixels = 0
-    for x in range(0, red_mask.shape[0]):
-        for y in range(0, red_mask.shape[1]):
-            if red_mask[x, y] != 0:
-                red_pixels += 1
+    red_pixels = np.sum(red_mask) / 255
 
     # Return if the pylon was detected or not
     if red_pixels >= RED_THRESHOLD:
