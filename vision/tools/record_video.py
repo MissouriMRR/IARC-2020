9c436029af810ea6e4e8b4e7d37d76101deba8f2
csdhv9 <csdhv9@umsystem.edu>
2020-03-06 16:19:38 -0600

    Update video recorder.

--- vision/tools/record_video.py
+++ vision/tools/record_video.py
@@ -1,5 +1,7 @@
 """
 Record a realsense video.
+
+When done, press ctrl-c once and give it a second to close up.
 """
 import os
 import pyrealsense2 as rs
@@ -22,8 +24,8 @@ if __name__ == '__main__':
     pipeline = rs.pipeline()
     config = rs.config()
 
-    config.enable_stream(rs.stream.depth, 1080, 720, 30)
-    config.enable_stream(rs.stream.color, 1080, 720, 30)
+    config.enable_stream(rs.stream.depth, 0, 0, rs.format.z16, 0)
+    config.enable_stream(rs.stream.color, 0, 0, rs.format.bgr8, 0)
 
     config.enable_record_to_file(FILENAME)
 
@@ -31,12 +33,19 @@ if __name__ == '__main__':
     pipeline.start(config)
 
     try:
+        i = 0
         while True:
+            i += 1
             frames = pipeline.wait_for_frames()
+
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
+
             if not depth_frame or not color_frame:
+                print("Failed to capture image.")
                 continue
 
-    finally:
-        pipeline.stop()
+    except Exception as e:
+        print(e)
+
+    pipeline.stop()
