aff5f9c5e45566540fe0bad4a2d92f4538ef9c3a
acbk2b <acbecker27@gmail.com>
2020-03-19 13:37:54 -0500

    Removed testing function and reformatted with black

--- flight/states/takeoff.py
+++ flight/states/takeoff.py
@@ -60,7 +60,6 @@ class Takeoff(State):
             if altitude >= 2:
                 return True
 
-
     async def takeoff(self, drone: System) -> None:
         logging.info("Taking off")
 
@@ -74,20 +73,32 @@ class Takeoff(State):
             curr_lat = round(gps.latitude_deg, 8)
             curr_lon = round(gps.longitude_deg, 8)
 
-            x = ( (target_lon - curr_lon) * 40000 * math.cos( (target_lat + curr_lat) * math.pi / 360) / 360 ) * 1000
+            x = (
+                (target_lon - curr_lon)
+                * 40000
+                * math.cos((target_lat + curr_lat) * math.pi / 360)
+                / 360
+            ) * 1000
 
-            y = ( (target_lat - curr_lat) * 40000 / 360) * 1000
+            y = ((target_lat - curr_lat) * 40000 / 360) * 1000
 
             # Calculate the degree to point the drone at
             try:
-                deg = round(((( math.atan(x / y) / math.pi) *180)))
+                deg = round((((math.atan(x / y) / math.pi) * 180)))
 
                 if y < 0:
                     z = 180
                     z = math.copysign(z, deg)
                     deg = z + deg
             except ZeroDivisionError:
-                deg = round(((( math.asin( x / ( math.sqrt(( x**2 ) + ( y**2 )))) / math.pi) * 180 )))
+                deg = round(
+                    (
+                        (
+                            (math.asin(x / (math.sqrt((x ** 2) + (y ** 2)))) / math.pi)
+                            * 180
+                        )
+                    )
+                )
 
                 if y < 0:
                     z = 180
@@ -101,15 +112,15 @@ class Takeoff(State):
 
             except ZeroDivisionError:
                 dx = math.copysign(
-                        35 * math.cos(math.asin(y / (math.sqrt((x**2) + (y**2))))), x
+                    35 * math.cos(math.asin(y / (math.sqrt((x ** 2) + (y ** 2))))), x
                 )
                 dy = math.copysign(
-                        35 * math.sin(math.asin(y / (math.sqrt((x**2) + (y**2))))), y
+                    35 * math.sin(math.asin(y / (math.sqrt((x ** 2) + (y ** 2))))), y
                 )
 
             # Start the drone pointing in the direction of the first pylon
             await drone.offboard.set_velocity_ned(
-                sdk.VelocityNedYaw(dy,dx,up_speed,deg)
+                sdk.VelocityNedYaw(dy, dx, up_speed, deg)
             )
 
             # Loops until the desired altitude has been attained,
@@ -119,34 +130,6 @@ class Takeoff(State):
                 # Leave it moving towards the first pylon,
                 # but set the upwared velocity to 0, so it stops going up
                 await drone.offboard.set_velocity_ned(
-                    sdk.VelocityNedYaw(dy,dx,0,deg)
+                    sdk.VelocityNedYaw(dy, dx, 0, deg)
                 )
                 return
-
-
-    async def test_takeoff(self, drone: System) -> None:
-        logging.info("------------Test--------------")
-
-        logging.info("Climbing")
-        await drone.offboard.set_velocity_body(
-                sdk.VelocityBodyYawspeed(0.0,0.0,-1.0,0.0)
-        )
-
-        await asyncio.sleep(10)
-
-        logging.info("Going back down")
-        await drone.offboard.set_velocity_body(
-                sdk.VelocityBodyYawspeed(0.0,0.0,1.0,0.0)
-        )
-
-        await asyncio.sleep(5)
-
-        logging.info("Hovering")
-        await drone.offboard.set_velocity_body(
-                sdk.VelocityBodyYawspeed(0.0,0.0,0.0,0.0)
-        )
-
-        await asyncio.sleep(3)
-
-        logging.info("Landing Drone")
-        await drone.action.land()
