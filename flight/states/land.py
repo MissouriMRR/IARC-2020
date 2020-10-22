32339fd81625fee1fe2de3c293cff5a9aa860d5d
Connor Hallemann <cjhr95@mst.edu>
2020-10-22 16:19:35 -0700

    Edited Manual Land Function

--- flight/states/land.py
+++ flight/states/land.py
@@ -24,14 +24,18 @@ class Land(State):
             current_altitude: float = round(position.relative_altitude_m, 3)
             if current_altitude > 0.5:
                 # 0.7 m/s is default velocity used in drone.action.land() command, can be changed if necessary
-                await drone.offboard.set_velocity_body(sdk.offboard.VelocityBodyYawspeed(0.0, 0.0, 0.7, 0.0))
+                await drone.offboard.set_velocity_body(
+                    sdk.offboard.VelocityBodyYawspeed(0.0, 0.0, 0.7, 0.0)
+                )
             elif 0.5 > current_altitude > 0:
-                await drone.offboard.set_velocity_body(sdk.offboard.VelocityBodyYawspeed(0.0, 0.0, 0.35, 0.0))
+                await drone.offboard.set_velocity_body(
+                    sdk.offboard.VelocityBodyYawspeed(0.0, 0.0, 0.35, 0.0)
+                )
             else:
-                await drone.offboard.set_velocity_body(sdk.offboard.VelocityBodyYawspeed(0.0, 0.0, 0.0, 0.0))
-                break
-        return
-
+                await drone.offboard.set_velocity_body(
+                    sdk.offboard.VelocityBodyYawspeed(0.0, 0.0, 0.0, 0.0)
+                )
+                return
 
     async def run(self, drone: System) -> State:
         """
