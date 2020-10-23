b1dc82dbc36b78a6f35dc1d8dfe8c63b3bfb673a
Elijah Wade <ewade913@gmail.com>
2020-10-22 22:16:16 -0500

    adding in early_laps a function that does not offset the fly to point and tell it to fly to launch before landing also custom point accuracy for function

--- flight/config.py
+++ flight/config.py
@@ -7,7 +7,7 @@ MAX_SPEED: float = 6.352  # m/s
 ALT_CORRECTION_SPEED: float = 0.25  # m/s down
 MAX_ALT: float = 9.0  # m
 TAKEOFF_ALT: float = 1.0  # m
-FLYING_ALT: float = 6.0 # m
+FLYING_ALT: float = 6.0  # m
 # What percentage of the hight can we loos/gain before unsafe
 ALT_PERCENT_ACCURACY: float = 0.15
 ALT_RANGE_MAX: float = FLYING_ALT + (FLYING_ALT * ALT_PERCENT_ACCURACY)  # m
@@ -52,7 +52,6 @@ async def config_params(drone: System):
 
     # Set RC loss failsafe mode HOLD
     await drone.param.set_param_int("NAV_RCL_ACT", 1)
-
     await drone.param.set_param_float("LNDMC_XY_VEL_MAX", 0.5)
     await drone.param.set_param_float("LNDMC_FFALL_THR", 3)
     await drone.param.set_param_float("LNDMC_FFALL_TTRI", 0.15)
