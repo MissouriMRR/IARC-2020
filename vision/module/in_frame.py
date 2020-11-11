dc794f20d0082f6b8a78501f00f16b91e20dbe98
Leah Edwards <leah.b.edwards@mst.edu>
2020-11-10 19:46:02 -0600

    i think these files are stopping me from pushing and if not i'm sorry

--- vision/module/in_frame.py
+++ vision/module/in_frame.py
@@ -10,7 +10,9 @@ import argparse
 # Constants
 BLUR_SIZE = 5  # Blur kernel size
 BUCKET_MODIFIER = 1  # Changes how many buckets are in the range
-MIN_SLOPES_IN_BUCKET = 15  # Minimum number of slopes in a single bucket to identify the module
+MIN_SLOPES_IN_BUCKET = (
+    15  # Minimum number of slopes in a single bucket to identify the module
+)
 MAX_CIRCLES = 100  # Maximum number of cirlces that can be detected in an image before ModuleInFrame fails
 
 
@@ -50,7 +52,16 @@ def ModuleInFrame(color_image: np.ndarray) -> bool:
     laplacian = np.uint8(laplacian)
 
     # Hough Circle Detection
-    circles = cv2.HoughCircles(image=laplacian, method=cv2.HOUGH_GRADIENT, dp=1, minDist=8, param1=75, param2=24, minRadius=0, maxRadius=50)
+    circles = cv2.HoughCircles(
+        image=laplacian,
+        method=cv2.HOUGH_GRADIENT,
+        dp=1,
+        minDist=8,
+        param1=75,
+        param2=24,
+        minRadius=0,
+        maxRadius=50,
+    )
     if circles is None:  # no circles found
         return False
     elif circles.shape[1] > MAX_CIRCLES:  # too many circles found
@@ -96,7 +107,7 @@ def ModuleInFrame(color_image: np.ndarray) -> bool:
     return any(buckets > MIN_SLOPES_IN_BUCKET)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     To test in_frame, use
     "python in_frame.py -i (image name).(image file extension)"
@@ -105,8 +116,10 @@ if __name__ == '__main__':
     or a path must be specified
     """
     # # Create object for parsing command-line options
-    parser = argparse.ArgumentParser(description="Read image file and display depth and test for ModuleInFrame.\
-                                     To read an image file, type \"python in_frame_driver.py --i (image name).(image extension)\"")
+    parser = argparse.ArgumentParser(
+        description='Read image file and display depth and test for ModuleInFrame.\
+                                     To read an image file, type "python in_frame_driver.py --i (image name).(image extension)"'
+    )
     # # Add argument which takes path to a bag file as an input
     parser.add_argument("-i", "--input", type=str, help="Path to the image file")
     # # Parse the command line arguments to an object
@@ -115,7 +128,7 @@ if __name__ == '__main__':
     if args.input:
         inputImageFile = args.input
     else:
-        inputImageFile = '../vision_images/module/Block2.jpg'
+        inputImageFile = "../vision_images/module/Block2.jpg"
         # raise FileNotFoundError("No input parameter has been given. For help type --help"
 
     image = cv2.imread(inputImageFile)
