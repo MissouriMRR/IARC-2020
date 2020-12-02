773dc60db923c4bdf5271ba41761e32a0bd155b9
Noah <nekgr8@mst.edu>
2020-12-01 19:05:57 -0600

    Added a 'yes' check in main function to wait for user input before starting takeoff

--- run.py
+++ run.py
@@ -19,6 +19,12 @@ def main() -> None:
     )
     args = parser.parse_args()
     logging.debug("Simulation flag %s", "enabled" if args.simulation else "disabled")
+    confirm = "no"
+    while(confirm != "yes"):
+        if(not args.simulation):
+            confirm = input("Enter 'yes' to initiate takeoff: ")
+        else:
+            confirm = "yes"
     run_threads(args.simulation)
 
 
