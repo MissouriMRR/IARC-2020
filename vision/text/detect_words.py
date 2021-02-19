5ef77d7bafe67d4057ec6d744fc3e59b0b65b50f
Andrew-Gibson42 <andrewgibs4242@gmail.com>
2021-02-18 22:28:08 -0600

    Fixed PR issues

--- vision/text/detect_words.py
+++ vision/text/detect_words.py
@@ -7,6 +7,7 @@ import numpy as np
 import cv2
 import os, sys
 
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 gparent_dir = os.path.dirname(parent_dir)
@@ -16,56 +17,75 @@ sys.path += [parent_dir, gparent_dir, ggparent_dir]
 from bounding_box import BoundingBox, ObjectType
 
 class TextDetector:
+    """
+    Finds the bounding boxes of the specified text in the frame.
+    """
     def __init__(self):
         self.text = "модулииртибот"
 
-    def detect_russian_word(self, color_image, depth_image = None):
+    def detect_russian_word(self, color_image: np.ndarray, depth_image: np.ndarray):
         """
         Detect words in given image.
 
+        Paramters
+        ------
+        color_image
+            color ndarray from the realsense camera
+        depth_image
+            depth ndarray from the realsense camera
+
         Returns
         -------
-        A list of box objects that contain desired text
+        A list of bounding box objects that contain desired text
         """
-        # Currently non-functional
-        # Idea was:
-        # Find the blue rectangle around the text
-        #                |
-        #                V
-        #      Get the contours of it
-        #               |
-        #               V
-        #   Select only the contours with children (noise filter: only the rectangle should have child contours)
-        #               |                                                   |
-        #               V                                                   V
-        #           Make a mask out of a contour                 Find the center of contour
-        #               |                   |                               |
-        #               V                   V                               |
-        #        Select Edges           Select Region of plain image        |
-        #              |                                   |                |
-        #              V                                   |                |
-        #   Run HoughLinesP on Edges                       |                |
-        #              |                                   |                |
-        #              V                                   |                |
-        #   Use trig to find rotation of rectangle         |                |
-        #                                     |            |                |
-        #                                     V            V                V
-        #                               Rotate region of image about center by found measure
-        #                                                  |
-        #                                                  V
-        #                           Pass into pyTesseract, does it have our russian words in it?
-        #                                    Yes                                    No
-        #                                     |                                      |
-        #                                     V                                      V
-        #                              More trig to return the                   Were there anymore contours with children?
-        #                          bounding box relative to the whole image          Yes                         No
-        #                                    |                                       |                           |
-        #                                    V                                       V                           V
-        #                              That's it, you done         Go back through process with them         Either the image has no
-        #                                                                                                    russian or it is too noisy
-        #
-        # This would probably be wildly unperformant, but I think it would make pytesseract understand the rotated text
+        sliced_rotated_image = self.get_rotated_min_area_rect(color_image, depth_image)
 
+        if sliced_rotated_image == []:
+            return []
+
+        tessdata = pytesseract.image_to_data(
+            sliced_rotated_image, output_type=pytesseract.Output.DICT, lang="uzb_cyrl"
+        )
+
+        n_boxes = len(tessdata["level"])
+        box_obs = []
+        contents = tessdata["text"]
+        for i in range(n_boxes):
+            if not contents[i]:
+                continue
+            else:
+                for j in contents[i]:
+                    if j in self.text:
+                        (x, y, w, h) = (
+                            tessdata["left"][i],
+                            tessdata["top"][i],
+                            tessdata["width"][i],
+                            tessdata["height"][i],
+                        )
+                        verts = [(x, y), (x + w, y), (x, y + h), (x + w, y + h)]
+                        cv2.rectangle(color_image, verts[0], verts[-1], (0, 255, 0), 2)
+                        box = BoundingBox(verts, ObjectType("text"))
+                        box_obs.append(box)
+                        break
+
+        return box_obs
+
+
+    def get_rotated_min_area_rect(self, color_image: np.ndarray, depth_image: np.ndarray):
+        """
+        Returns min area rect of inside the tape
+
+        Parameters
+        -----
+        color_image
+            color ndarray from the realsense camera
+        depth_image
+            depth ndarray from the realsense camera
+
+        Returns
+        --------
+        min area rect
+        """
         # the text is always in a blue rectangle. this finds the rectangle
         # remove noise, int16 to prevent negative overflow in following step
         blur_image = np.int16(cv2.GaussianBlur(color_image, (5, 5), 0))
@@ -80,9 +100,7 @@ class TextDetector:
         # make blue parts black, rest white
         mask_image = np.where(isblue, np.uint8(0), np.uint8(255))
 
-        # if we have depth data, get rid of irrelevant far aways stuff like the sky
-        if depth_image is not None:
-            mask_image = np.where((depth_image < 8000), mask_image, np.uint8(255))
+        mask_image = np.where((depth_image < 8000), mask_image, np.uint8(255))
 
         edges = cv2.Canny(mask_image, 250, 255)
         # canny is too good at its job, soften it up a bit for houghlines and findcontours
@@ -97,7 +115,7 @@ class TextDetector:
         )
 
         if hierarchy is None:
-            return None, None
+            return []
 
         largestArea = 0
         largestContour = 0
@@ -105,14 +123,12 @@ class TextDetector:
             if cv2.contourArea(c) > largestArea:
                 largestContour = c
 
+        # contourAreas = np.array([cv2.contourArea(c) for c in contours])
+        # largestContour = contours[np.argmax(contourAreas)]
+
         x,y,w,h = cv2.boundingRect(largestContour)
 
         rect = cv2.minAreaRect(largestContour)
-
-        # stuff for viewing the min area rect, if desired
-        # box = cv2.boxPoints(rect)
-        # box = np.int0(box)
-        # color_image = cv2.drawContours(color_image,[box],0,(0,0,255),2)
 
         center, size, theta = rect
         
@@ -129,72 +145,24 @@ class TextDetector:
         dst = cv2.warpAffine(color_image, rot_matrix, color_image.shape[:2])
         sliced_rotated_image = cv2.getRectSubPix(dst, size, center)
 
-        cv2.imshow("Image", sliced_rotated_image)
+        return sliced_rotated_image
+
+
+    def visualize_min_area_rect(self, color_image: np.ndarray, depth_image: np.ndarray):
+        """
+        Allows for visualization of the rotated min area rect detect_words is using
+
+        Parameters
+        -----
+        color_image
+            color ndarray from the realsense camera
+        depth_image
+            depth ndarray from the realsense camera
+        """
+        sliced_rotated_image = self.get_rotated_min_area_rect(color_image, depth_image)
+
+        cv2.imshow("MinAreaRect", sliced_rotated_image)
         cv2.waitKey(0)
-
-        # -------- lines for viewing contours ---------------
-
-        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 250, 600, 20)
-
-        # make it a color image again so we can draw colored lines on it
-        mask_image = np.stack((mask_image,) * 3, axis=2)
-
-
-        if lines is not None:
-            for i in range(lines.shape[0]):
-                for x1, y1, x2, y2 in lines[i]:
-                    mask_image = cv2.line(
-                        mask_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=2
-                    )
-    
-        # cv2.imshow("img", mask_image)
-        # cv2.waitKey(0)
-
-        # ----------------------------------------------------
-
-        # not only does tesseract basically require uint8, but apparently it has to be
-        # thresholded as uint8 or tesseract will throw a fit
-        # filter_image = np.uint8(filter_image)
-        # _, filter_image = cv2.threshold(filter_image, 0, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY)
-        # _, filter_image = cv2.threshold(filter_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        # for whatever reason pytesseract needs images in rgb form, so shape has to be (width, height, 3)
-        # filter_image = np.stack((filter_image,)*3, axis=2)
-
-        # shows what the filtered image looks like
-        # cv2.imshow('img', filter_image)
-        # cv2.waitKey(0)
-
-        ## only return boxes that have text in them
-        ## eg. find a way to check if boxes are repetitive or do not contain text
-
-        tessdata = pytesseract.image_to_data(
-            sliced_rotated_image, output_type=pytesseract.Output.DICT, lang="uzb_cyrl"
-        )
-
-        n_boxes = len(tessdata["level"])
-        box_obs = []
-        contents = tessdata["text"]
-        for i in range(n_boxes):
-            if not contents[i]:
-                continue
-            else:
-                for j in contents[i]:
-                    if j in self.text:
-                        # print(contents[i])
-                        (x, y, w, h) = (
-                            tessdata["left"][i],
-                            tessdata["top"][i],
-                            tessdata["width"][i],
-                            tessdata["height"][i],
-                        )
-                        # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                        verts = [(x, y), (x + w, y), (x, y + h), (x + w, y + h)]
-                        cv2.rectangle(color_image, verts[0], verts[-1], (0, 255, 0), 2)
-                        box = BoundingBox(verts, ObjectType("text"))
-                        box_obs.append(box)
-                        break
-
-        return box_obs, mask_image
 
 
 if __name__ == "__main__":
@@ -202,40 +170,6 @@ if __name__ == "__main__":
     import os
     import argparse
     from common import box_plotter
-
-    # # sample_dir = os.path.join("vision_images", "text", "Feb29")
-    # sample_dir = "../vision_images/text/Feb29"
-    # sample_ims = []
-
-    # for filename in os.listdir(sample_dir):
-    #     if filename.endswith(".jpg") or filename.endswith(".png"):
-    #         sample_ims.append(os.path.join(sample_dir, filename))
-    #     else:
-    #         continue
-
-    # times = []
-
-    # for i in sample_ims:
-
-    #     color_image = cv2.imread(i)
-    #     depth_image = np.load(i.split("colorImage")[0] + "depthImage.npy")
-
-    #     if color_image is None:
-    #         raise FileNotFoundError("Could not read image!")
-
-    #     start = time.time()
-
-    #     detector = TextDetector()
-    #     result, mask_image = detector.detect_russian_word(color_image, depth_image)
-
-    #     times.append(time.time() - start)
-    #     # box_plotter.plot_box(result, mask_image)
-
-    # print("Max: ", np.max(times))
-    # print("Min: ", np.min(times))
-    # print("Mean: ", np.mean(times))
-
-    # --------------------------------------------------------------
 
     # # Create object for parsing command-line options
     parser = argparse.ArgumentParser(
@@ -251,14 +185,16 @@ if __name__ == "__main__":
         inputImageFile = args.input
     else:
         inputImageFile = "../vision_images/module/Block2.jpg"
-        # raise FileNotFoundError("No input parameter has been given. For help type --help"
 
     color_image = cv2.imread(inputImageFile + "-colorImage.jpg")
+    depth_image = np.load(inputImageFile + "-depthImage.npy")
 
     if color_image is None:
         raise FileNotFoundError("Could not read image!")
 
     detector = TextDetector()
-    result, mask_image = detector.detect_russian_word(color_image)
+    detector.visualize_min_area_rect(color_image, depth_image)
+    result = detector.detect_russian_word(color_image, depth_image)
+    print (result)
 
     # box_plotter.plot_box(result, mask_image)
