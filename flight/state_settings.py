f86f55f08af5e60d2af8715432c901b9afe8d773
pieperm <mepww2@umsystem.edu>
2021-03-20 04:08:28 -0500

    Modify StateSettings to allow switching to SimpleTakeoff (normal takeoff by default)

--- flight/state_settings.py
+++ flight/state_settings.py
@@ -10,6 +10,8 @@ class StateSettings:
         self.num_early_laps: int = DEFAULT_EARLY_LAPS
         self.go_to_mast: bool = True
         self.detect_module: bool = False
+        self.simple_takeoff: bool = False
+        self.vision_test_type: str = "module"
 
     def set_early_laps(self, do_early_laps: bool) -> None:
         """Setter for whether to do early laps"""
@@ -26,3 +28,21 @@ class StateSettings:
     def set_detect_module(self, detect_module: bool) -> None:
         """Setter for whether to detect the module"""
         self.detect_module = detect_module
+
+    def enable_simple_takeoff(self, simple_takeoff: bool) -> None:
+        """
+        Setter for whether to perform simple takeoff instead of regular takeoff
+            simple_takeoff(bool): True for drone to go straight up, False to behave normally
+        """
+        self.simple_takeoff = simple_takeoff
+
+    def set_vision_test(self, test_type: str) -> None:
+        """
+        Setter for the type of vision test to run
+        This should only generally only be used with simple takeoff
+            test_type(str) 'module' for module detection or 'text' for mast text detection
+        """
+        if test_type == "module" or test_type == "text":
+            self.vision_test_type = test_type
+        else:
+            raise ValueError(f"test_type must be 'module' or 'text', got {test_type}")
